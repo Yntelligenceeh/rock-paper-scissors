cb047158c6199187b85706af31fa4ee74d9b1f48
Yntelligenceeh <132067787+Yntelligenceeh@users.noreply.github.com>
2023-08-09 14:31:27 +0200

    Add function to get the player's choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,5 +1,11 @@
-function getComputerChoice() {
-    let options = ["Rock", "Paper", "Scissors"];
-    let option = options[Math.floor(Math.random() * options.length)]
-    return option;
+function getPlayerChoice() {
+    const playerOptions = ["rock", "paper", "scissors"];
+    let playerPick = prompt("Pick rock, paper or scissors", "rock");
+    
+    if(playerOptions.includes(playerPick.toLowerCase())) {
+        console.log("Player picked: " + playerPick);
+        return playerPick;
+    }
+    else return "Invalid selection, please only pick rock, paper or scissors.";
 }
+
